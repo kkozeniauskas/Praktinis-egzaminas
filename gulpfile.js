f5d1db14276d29fe9105d2bbe6ab092c4fa288c6
kkozeniauskas <k.kozeniauskas@gmail.com>
2023-05-07 20:44:31 +0300

    pricing first update

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,32 @@
 "use strict"
 
+// nav burger
+const burger = document.querySelector("#burger");
+const links = document.querySelector(".links");
 
+burger.addEventListener("click", () =>{
+  links.classList.toggle("absolute");
+  burger.classList.toggle("rotate");
+});
+
+// swiper
+
+const swiper = new Swiper('.swiper',{
+    direction: 'horizontal',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        1000: {
+            slidesPerView: 3
+        }
+    }
+  });
