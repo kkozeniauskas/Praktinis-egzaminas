98b4bc022c953f150b4c963e39d78d59df5659cb
kkozeniauskas <k.kozeniauskas@gmail.com>
2023-05-08 15:30:00 +0300

    mobile version social form and aside

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 "use strict"
 
+
 // nav burger
 const burger = document.querySelector("#burger");
 const links = document.querySelector(".links");
@@ -9,24 +10,6 @@ burger.addEventListener("click", () =>{
   burger.classList.toggle("rotate");
 });
 
-// swiper
 
-const swiper = new Swiper('.swiper',{
-    direction: 'horizontal',
-    loop: true,
-  
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
 
-    breakpoints: {
-        1000: {
-            slidesPerView: 3
-        }
-    }
-  });
+  
